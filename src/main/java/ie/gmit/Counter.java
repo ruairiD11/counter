01fc58ac384c5ca68ede415ddba680fa2857e254
RUAIRI DOHERTY - STUDENT <G00352092@gmit.ie>
2020-02-13 11:43:55 +0000

    First commit

---
+++ src/main/java/ie/gmit/Counter.java
@@ -0,0 +1,13 @@
+package ie.gmit;
+
+public class Counter {
+    private int count;
+
+    public Counter() {
+        this.count = 0;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
---
+++ src/test/java/ie/gmit/CounterTest.java
@@ -0,0 +1,15 @@
+package ie.gmit;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CounterTest {
+
+    Counter myCounter;
+
+    @Test
+    void testConstructor()
+    {
+        myCounter = new Counter();
+        assertEquals(0, myCounter.getCount());
+    }
+}
